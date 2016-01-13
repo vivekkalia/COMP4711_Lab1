1e1045485838552a9c67b55073b42a1670965419
Vivek Kalia <vkalia92@gmail.com>
2016-01-12 16:24:56 -0800

    fixed file name

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+        $name  = "Vivek";
+        $what  = "geek";
+        $level = 10;
+        echo 'Hi, my name is ' . $name . ' and I am a level ' . $level . ' ' . $what;
+
+        $hoursworked = 10;
+        $rate = 12;
+        $total = $hoursworked * $rate;
+        echo "<br />You owe me " . $total;
+    ?>
+</body>
+</html>
