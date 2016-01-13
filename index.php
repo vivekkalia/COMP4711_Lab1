cdf33076d757f173f32323f0502400eb840f86f0
Vivek Kalia <vkalia92@gmail.com>
2016-01-12 17:04:56 -0800

    created first winner function

--- index.php
+++ index.php
@@ -7,23 +7,54 @@
     <body>
         <?php
 
-        $name  = "Jim";
-        $what  = "geek";
-        $level = 10;
-        echo 'Hi, my name is ' . $name . ' and I am a level ' . $level . ' ' . $what;
-        echo '<br />';
-
-        $hoursworked = $_GET['hours'];
-        $rate = 12;
-
-        if($hoursworked > 40) {
-            $total = $hoursworked * $rate *1.5;
-        } else {
-            $total = $hoursworked * $rate;
+        $position = $_GET['board'];
+        $squares = str_split("$position");
+        if(winner('o',$squares)) {
+            echo 'you win!';
         }
 
-        echo ($total > 0) ? 'You owe me ' . $total : "You're welcome!";
 
         ?>
 </body>
 </html>
+
+<?php
+
+    function winner($token,$position) {
+        $won = false;
+        if(( $position[0] == $token) &&
+            ($position[1] == $token) &&
+            ($position[2] == $token)) {
+                $won = true;
+        } else if(($position[3] == $token) &&
+            ($position[4] == $token) &&
+            ($position[5] == $token)) {
+                $won = true;
+        } else if(($position[6] == $token) &&
+            ($position[7] == $token) &&
+            ($position[8] == $token)) {
+                $won = true;
+        } else if(($position[0] == $token) &&
+            ($position[3] == $token) &&
+            ($position[6] == $token)) {
+                $won = true;
+        } else if(($position[1] == $token) &&
+            ($position[4] == $token) &&
+            ($position[7] == $token)) {
+            $won = true;
+        } else if(($position[2] == $token) &&
+            ($position[5] == $token) &&
+            ($position[8] == $token)) {
+            $won = true;
+        } else if(($position[0] == $token) &&
+            ($position[4] == $token) &&
+            ($position[8] == $token)) {
+                $won = true;
+        } else if(($position[2] == $token) &&
+            ($position[4] == $token) &&
+            ($position[6] == $token)) {
+            $won = true;
+        }
+        return $won;
+    }
+?>
